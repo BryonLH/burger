332a4467c37a230ce88bba5d92650b937c0ef02e
Bryon <bryonlh@gmail.com>
2019-08-25 16:49:34 -0700

    creating orm and connection js files

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function() {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function(err, result) {
+          if (err) throw err;
+          console.log(result);
+        });
+      },
+    
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Double with Cheese", false),
+("Westeran Bacon Cheese Burger", false),
+("Vegan Burger", false);
